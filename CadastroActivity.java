ba85c3f3b44be2fcc9cd0d0c7680d5edb67d856e
joaogus19 <110412834+gus0199@users.noreply.github.com>
2025-06-21 15:54:39 -0300

    Update CadastroActivity.java

--- CadastroActivity.java
+++ CadastroActivity.java
@@ -8,6 +8,44 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imgProduto;
+    Button btnFoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        imgProduto = findViewById(R.id.imgProduto);
+        btnFoto = findViewById(R.id.btnFoto);
+
+        btnFoto.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap foto = (Bitmap) extras.get("data");
+            imgProduto.setImageBitmap(foto);
+        }
+    }
+}
+
 public class CadastroActivity extends AppCompatActivity {
 
     EditText edtNome, edtQuantidade, edtPreco;
